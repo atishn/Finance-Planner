a8a669f6477e617e704497f9351e197a64bf8e73
Atish Narlawar <atish.narlawar@gmail.com>
2013-09-10 14:06:15 -0400

    Added Delete confirmation check

---
+++ hr/static/js/hugeplanner.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: anarlawar
+ * Date: 9/10/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+function confirmClick(e){
+
+    if(confirm("Are you Sure?")){
+
+    }else{
+        e.preventDefault();
+    }
+}
