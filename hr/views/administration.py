0a9b923c82080e92b54f8736947dfd4e34fbc39e
Atish Narlawar <atish.narlawar@gmail.com>
2013-09-16 11:22:47 -0400

    Added expense history in admin expense page

--- hr/views/administration.py
+++ hr/views/administration.py
@@ -132,7 +132,6 @@ def administration_revenue(request):
             quarter_end_date = datetime.date(long(quarter_end_year_text), long(quarter_end_date_parts[0]),
                                              long(quarter_end_date_parts[1]))
 
-
             for project in account.projects:
                 revenue_local = long(request.params.get(str(project.id) + "-revenue"))
 
@@ -171,7 +170,7 @@ def administration_expenses_offices(request):
         account = DBSession.query(Account).filter_by(id=account_id).first()
         user = DBSession.query(User).filter_by(id=user_id).first()
         year = str(datetime.datetime.now().year)
-
+        quarter_end_year_text = None
         if user is None or account is None or user.is_administrator == False:
             return HTTPFound(request.application_url)
 
@@ -185,12 +184,12 @@ def administration_expenses_offices(request):
 
             for office in account.offices:
 
-                if request.params.get(str(office.id) + "-local")!= "":
+                if request.params.get(str(office.id) + "-local") != "":
                     expense_local_lc = long(request.params.get(str(office.id) + "-local"))
                 else:
                     expense_local_lc = 0
 
-                if request.params.get(str(office.id) + "-global")!= "":
+                if request.params.get(str(office.id) + "-global") != "":
                     expense_global_lc = long(request.params.get(str(office.id) + "-global"))
                 else:
                     expense_global_lc = 0
@@ -211,10 +210,54 @@ def administration_expenses_offices(request):
                 else:
                     actual_expense = ActualExpense(office, None, expense_local, expense_global, quarter_end_date)
                     DBSession.add(actual_expense)
-                DBSession.flush()
+
+        DBSession.flush()
+
+        usd_to_local = 1
+        if user.currency is not None:
+            usd_to_local = user.currency.usd_to_currency
+
+        if quarter_end_year_text is not None:
+            expense_year = int(quarter_end_year_text)
+        else:
+            expense_year = int(year)
+
+        all_office_expense_sga = []
+        all_office_expense_salary_global = []
+        for office in account.offices:
+            office_expense_sga = [0, 0, 0, 0]
+            office_expense_salary_global = [0, 0, 0, 0]
+
+            for actual_expense in office.actual_expenses:
+                if actual_expense.quarter_end_date.year == expense_year:
+
+                    if actual_expense.quarter_end_date.month == 3:
+                        if actual_expense.expense_local is not None:
+                            office_expense_sga[0] = actual_expense.expense_local * usd_to_local
+                        if actual_expense.expense_global is not None:
+                            office_expense_salary_global[0] = actual_expense.expense_global * usd_to_local
+                    elif actual_expense.quarter_end_date.month == 6:
+                        if actual_expense.expense_local is not None:
+                            office_expense_sga[1] = actual_expense.expense_local * usd_to_local
+                        if actual_expense.expense_global is not None:
+                            office_expense_salary_global[1] = actual_expense.expense_global * usd_to_local
+                    elif actual_expense.quarter_end_date.month == 9:
+                        if actual_expense.expense_local is not None:
+                            office_expense_sga[2] = actual_expense.expense_local * usd_to_local
+                        if actual_expense.expense_global is not None:
+                            office_expense_salary_global[2] = actual_expense.expense_global * usd_to_local
+                    elif actual_expense.quarter_end_date.month == 12:
+                        if actual_expense.expense_local is not None:
+                            office_expense_sga[3] = actual_expense.expense_local * usd_to_local
+                        if actual_expense.expense_global is not None:
+                            office_expense_salary_global[3] = actual_expense.expense_global * usd_to_local
+
+            all_office_expense_sga.append(office_expense_sga)
+            all_office_expense_salary_global.append(office_expense_salary_global)
 
         return dict(logged_in=authenticated_userid(request), header=Header("administration"), account=account,
-                    user=user, year=year)
+                    user=user, year=year, all_office_expense_sga=all_office_expense_sga,
+                    all_office_expense_salary_global=all_office_expense_salary_global, expense_year=expense_year)
     except:
         traceback.print_exc()
         return HTTPFound(request.application_url)
@@ -244,7 +287,8 @@ def administration_expenses_clients(request):
                                              long(quarter_end_date_parts[1]))
 
             for client in account.clients:
-                if request.params.get(str(client.id) + "-expense") is not None and request.params.get(str(client.id) + "-expense") != "":
+                if request.params.get(str(client.id) + "-expense") is not None and request.params.get(
+                                str(client.id) + "-expense") != "":
                     expense_lc = long(request.params.get(str(client.id) + "-expense"))
                 else:
                     expense_lc = 0
@@ -255,13 +299,13 @@ def administration_expenses_clients(request):
                     expense_local = expense_lc * user.currency.currency_to_usd
 
                 actual_expense = DBSession.query(ActualExpense).filter_by(client_id=client.id).filter_by(
-                        quarter_end_date=quarter_end_date).first()
+                    quarter_end_date=quarter_end_date).first()
 
                 if actual_expense is not None:
-                        actual_expense.expense_local += expense_local
+                    actual_expense.expense_local += expense_local
                 else:
-                        actual_expense = ActualExpense(None, client, expense_local, None, quarter_end_date)
-                        DBSession.add(actual_expense)
+                    actual_expense = ActualExpense(None, client, expense_local, None, quarter_end_date)
+                    DBSession.add(actual_expense)
 
             DBSession.flush()
 
