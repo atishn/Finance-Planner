32a8785be7e010d91407641411a90cf2d3a0cc2b
Atish Narlawar <atish.narlawar@gmail.com>
2013-12-03 14:16:49 -0500

    Cursor loading

---
+++ hr/static/js/login.js
@@ -0,0 +1,10 @@
+/**
+ * User: anarlawar
+ * Date: 9/10/13
+ * Time: 1:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(function() {
+    $('#edit-name').focus();
+});
